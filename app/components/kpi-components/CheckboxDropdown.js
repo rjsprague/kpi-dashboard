266694f3fc957248c41d1b67792a144d8b297213
Ryan Sprague <rj.sprague@gmail.com>
2023-07-06 10:42:24 +0900

    Added search to CheckboxDropdown Added keyboard controls to CheckboxDropdown

--- app/components/kpi-components/CheckboxDropdown.js
+++ app/components/kpi-components/CheckboxDropdown.js
@@ -7,10 +7,15 @@ import LoadingIcon from "../LoadingIcon";
 
 
 function CheckboxDropdown({ options, onOptionSelected, selectedOptions, queryId, isSingleSelect, isLoadingData }) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [highlightedIndex, setHighlightedIndex] = useState(-1);
     const [isOpen, setIsOpen] = useState(false);
     const [contentHeight, setContentHeight] = useState(0);
+    const [filteredOptions, setFilteredOptions] = useState(options);
     const dropdownRef = useRef(null);
     const dropdownContentRef = useRef(null);
+    const searchInputRef = useRef(null);
+
 
     // console.log("selectedOptions", selectedOptions)
     // console.log("options ", options)
@@ -21,6 +26,9 @@ function CheckboxDropdown({ options, onOptionSelected, selectedOptions, queryId,
             if (contentElement) {
                 setContentHeight(contentElement.scrollHeight);
             }
+            searchInputRef.current && searchInputRef.current.focus();
+            contentElement.scrollTop = 0;
+            setHighlightedIndex(-1);
         } else {
             setContentHeight(0);
         }
@@ -40,37 +48,73 @@ function CheckboxDropdown({ options, onOptionSelected, selectedOptions, queryId,
         };
     }, []);
 
+    useEffect(() => {
+        if (searchTerm === "") {
+            setFilteredOptions(options);
+        } else {
+            const lowercasedSearchTerm = searchTerm.toLowerCase();
+            setFilteredOptions(options.filter(option => option.toLowerCase().includes(lowercasedSearchTerm)));
+        }
+    }, [options, searchTerm]);
+
+    useEffect(() => {
+        const optionElements = dropdownRef.current.querySelectorAll('li');
+        if (optionElements[highlightedIndex]) {
+            optionElements[highlightedIndex].scrollIntoView({ block: 'nearest' });
+        }
+    }, [highlightedIndex]);
+
     const toggleOpen = () => {
         setIsOpen(!isOpen);
     };
 
     const handleCheckboxChange = (option) => {
         let newSelectedOptions = [...selectedOptions];
-    
+
         if (isSingleSelect) {
             newSelectedOptions = [option];
         } else if (option === 'All') {
-            // When 'All' is selected, if all options are already selected, deselect all. Otherwise, select all.
             newSelectedOptions = newSelectedOptions.length === options.length ? [] : [...options];
         } else {
             const isSelected = selectedOptions.includes(option);
-    
+
             if (newSelectedOptions.length === options.length) {
-                // If all options are selected, and the user tries to select another option,
-                // deselect all options and only select the new option
                 newSelectedOptions = [option];
             } else if (isSelected) {
-                // If the option is already selected, deselect it
                 newSelectedOptions = newSelectedOptions.filter(selectedOption => selectedOption !== option);
             } else {
-                // Otherwise, add the option to the selected options
                 newSelectedOptions.push(option);
             }
         }
-    
         onOptionSelected(newSelectedOptions, queryId);
+        setSearchTerm("");
+        setHighlightedIndex(-1);
     };
-    
+
+    const handleKeyDown = (event) => {
+        switch (event.key) {
+            case 'ArrowDown':
+                event.preventDefault();
+                setHighlightedIndex(prevIndex => (prevIndex + 1) % filteredOptions.length);
+                break;
+            case 'ArrowUp':
+                event.preventDefault();
+                setHighlightedIndex(prevIndex => (prevIndex - 1 + filteredOptions.length) % filteredOptions.length);
+                break;
+            case 'Enter':
+                event.preventDefault();
+                if (highlightedIndex !== -1) {
+                    handleCheckboxChange(filteredOptions[highlightedIndex]);
+                }
+                break;
+            case 'Escape':
+                event.preventDefault();
+                setIsOpen(false);
+                break;
+            default:
+                break;
+        }
+    };
 
     const duration = 250;
     const defaultStyle = {
@@ -111,6 +155,15 @@ function CheckboxDropdown({ options, onOptionSelected, selectedOptions, queryId,
                             overflowY: "auto",
                         }}
                     >
+                        <input
+                            ref={searchInputRef}
+                            type="text"
+                            value={searchTerm}
+                            onChange={e => setSearchTerm(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                            className="w-full px-3 py-1 text-white bg-blue-800 rounded-md"
+                            placeholder="Search..."
+                        />
                         <ul className="py-1">
                             <li className="px-3 py-1 text-white cursor-pointer hover:bg-blue-800">
                                 <label className="inline-flex items-center">
@@ -124,22 +177,24 @@ function CheckboxDropdown({ options, onOptionSelected, selectedOptions, queryId,
                                     All
                                 </label>
                             </li>
-                            {Array.isArray(options) ? options.map((option) => {
-                                return (
-                                    <li key={option} className="px-3 py-1 text-white cursor-pointer hover:bg-blue-800">
-                                        <label className="inline-flex items-center">
-                                            <input
-                                                type="checkbox"
-                                                className="w-3 h-3 mr-2 text-blue-900 border-gray-300 rounded"
-                                                checked={selectedOptions?.includes(option)}
-                                                onChange={() => handleCheckboxChange(option)}
-                                                value={option}
-                                            />
-                                            {option}
-                                        </label>
-                                    </li>
-                                );
-                            }) : <LoadingIcon />}
+                            {Array.isArray(filteredOptions) ? filteredOptions.map((option, index) => (
+                                <li
+                                    key={option}
+                                    className={`px-3 py-1 text-white cursor-pointer hover:bg-blue-800 ${index === highlightedIndex ? 'bg-blue-800' : ''}`}
+                                    onClick={() => handleCheckboxChange(option)}
+                                >
+                                    <label className="inline-flex items-center">
+                                        <input
+                                            type="checkbox"
+                                            className="w-3 h-3 mr-2 text-blue-900 border-gray-300 rounded"
+                                            checked={selectedOptions?.includes(option)}
+                                            onChange={() => handleCheckboxChange(option)}
+                                            value={option}
+                                        />
+                                        {option}
+                                    </label>
+                                </li>
+                            )) : <LoadingIcon />}
                         </ul>
                     </div>
                 )}
