3a000399ce1bc84781de4d80e9d0740dac9f2f79
Ryan Sprague <rj.sprague@gmail.com>
2023-08-12 01:27:05 +0900

    Fixed QueryPanel data error Updated dataTable and RightSlideModal to handle side by side tables

--- app/components/kpi-components/QueryPanel.js
+++ app/components/kpi-components/QueryPanel.js
@@ -15,13 +15,11 @@ export default function QueryPanel({ query, height, setHeight, handleToggleQuery
     const clientSpaceId = useSelector(selectSpaceId);
 
     const { data: clients, error } = useSWR('/api/spaces',fetchClients);
-    console.log("clients", clients)
     // reverse the clients object so that the spaceid is the key and the name is the value
-    const clientsMap = Object.entries(clients).reduce((acc, [key, value]) => {
+    const clientsMap = clients && Object.entries(clients).reduce((acc, [key, value]) => {
         acc[value] = key;
         return acc;
     }, {});
-    console.log("clientsMap", clientsMap)
 
     return (
         <div className="p-2 text-sm rounded-lg shadow-super-3 bg-gradient-to-r from-blue-600 via-blue-800 to-blue-500 text-gray-50">
@@ -48,7 +46,7 @@ export default function QueryPanel({ query, height, setHeight, handleToggleQuery
                 </button>
                 
                 <div className='flex flex-col items-center gap-2 sm:flex-row'>
-                {clientsMap[clientSpaceId]}
+                {clients && clientsMap && clientsMap[clientSpaceId]}
                     {children}
                 </div>
                 <div className='flex flex-col justify-between gap-2 xs:flex-row'>
