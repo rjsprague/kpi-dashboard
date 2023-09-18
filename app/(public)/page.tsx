79ae65b13311c89524ba82c941549bdf6a436583
Ryan Sprague <rj.sprague@gmail.com>
2023-09-18 13:08:57 +0900

    Updated homepage logo

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -11,7 +11,7 @@ function Home() {
         <section className='flex flex-col items-center gap-20 px-10 mx-auto'>
           <div>
             <Image
-              src="/reia-logo.png"
+              src="/login-logo.svg"
               alt="REI Automated Logo"
               height={100}
               width={500}
