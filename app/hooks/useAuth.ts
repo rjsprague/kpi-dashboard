c691e4439be420e1f9861be9e278d4d7dfebd0b1
Ryan Sprague <rj.sprague@gmail.com>
2023-10-25 21:31:40 +0900

    Fixed additional Typescript issues in useAuth

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,39 +1,44 @@
 // useAuth.ts
-
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
 import Cookies from 'js-cookie';
 
 type Auth = {
-  token?: string;
-  tokenExpiry?: string;
+    token?: string;
+    tokenExpiry?: string;
 };
 
 type UseAuthReturn = {
-  auth: Auth | null;
-  setAuth: (auth: Auth) => void;
-  logout: () => void;
-  user: any;  // Define the user type here based on your user object shape
-  loading: boolean;
-  handleLogin: (credentials: any) => void; // Define the credentials type
-  updateUser: (user: any) => void; // Define the user update type
+    auth: Auth | null;
+    setAuth: (auth: Auth) => void;
+    logout: () => void;
+    user: any;
+    loading: boolean;
+    handleLogin: (email: string, password: string) => Promise<void>;
+    updateUser: (user: any) => void;
 };
 
 const useAuth = (): UseAuthReturn => {
-  const { auth: originalAuth, setAuth, logout, user, loading, handleLogin, updateUser } = useContext(AuthContext);
+    const context = useContext(AuthContext);
 
-  let updatedAuth: Auth | null = originalAuth;
-
-  // If auth.token is undefined, try to get it from a cookie
-  if (!originalAuth?.token) {
-    const token = Cookies.get('token');
-    const tokenExpiry = Cookies.get('tokenExpiry');
-    if (token) {
-      updatedAuth = { ...originalAuth, token: token, tokenExpiry: tokenExpiry };
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
     }
-  }
 
-  return { auth: updatedAuth, setAuth, logout, user, loading, handleLogin, updateUser };
+    const { auth: originalAuth, setAuth, logout, user, loading, handleLogin, updateUser } = context;
+
+    let updatedAuth: Auth | null = originalAuth;
+
+    // If auth.token is undefined, try to get it from a cookie
+    if (!originalAuth?.token) {
+        const token = Cookies.get('token');
+        const tokenExpiry = Cookies.get('tokenExpiry');
+        if (token) {
+            updatedAuth = { ...originalAuth, token: token, tokenExpiry: tokenExpiry };
+        }
+    }
+
+    return { auth: updatedAuth, setAuth, logout, user, loading, handleLogin, updateUser };
 };
 
 export default useAuth;
