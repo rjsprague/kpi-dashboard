2d46ff99901da777841a48764d8a22212f50a0b6
Ryan Sprague <rj.sprague@gmail.com>
2023-12-21 15:17:34 +0900

    Added Jira Service Management widget to app

--- app/(private)/layout.js
+++ app/(private)/layout.js
@@ -7,6 +7,7 @@ import { ReduxProvider } from '../GlobalRedux/provider';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export const fetchCache = 'force-no-store';
 
@@ -14,8 +15,15 @@ export default function RootLayout({ children }) {
 
     return (
         <AuthProvider>
+            <head>
+                <script
+                    id="jira-service-desk-widget"
+                    data-jsd-embedded data-key="1b3385f6-3fa9-43d0-8209-b36bcd93b5fd"
+                    data-base-url="https://jsd-widget.atlassian.com"
+                    src="https://jsd-widget.atlassian.com/assets/embed.js"
+                    ></script>
+            </head>
             <html lang="en">
-                <head />
                 <body className='absolute inset-0 h-screen max-h-screen overflow-hidden'>
                     <ReduxProvider>
                         <div className=''>
@@ -32,6 +40,7 @@ export default function RootLayout({ children }) {
                     </ReduxProvider>
                 </body>
                 <Script src="//scripts.reiautomated.io/wp-content/plugins/gravity-forms-iframe-master/assets/scripts/gfembed.min.js"></Script>
+
             </html>
         </AuthProvider>
     )
